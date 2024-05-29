f19b031af5f786a3cf43d02fc53a700d3d0ae73c
Caarlos645 <casanco2000@gmail.com>
2024-05-30 01:19:53 +0200

    aplicando cambios

--- src/test/java/com/cursoudemy/ecommerce/backend/BackendApplicationTests.java
+++ src/test/java/com/cursoudemy/ecommerce/backend/BackendApplicationTests.java
@@ -3,7 +3,9 @@ package com.cursoudemy.ecommerce.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.proyecto.ecommerce.backend.BackendApplication;
+
+@SpringBootTest(classes = BackendApplication.class)
 class BackendApplicationTests {
 
 	@Test
